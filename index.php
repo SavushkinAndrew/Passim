1528c6eafb90a154f2597cc7687af6718d7755da
Gortek2214 <nikita.borisov.36@gmail.com>
2017-04-25 21:49:45 +0300

    test get

--- index.php
+++ index.php
@@ -6,8 +6,11 @@
     <title>Document</title>
 </head>
 <body>
+
  <?php 
+   $_GET["q"] = str_replace(' ', '&shy;', $_GET["q"]);
     if (isset($_GET["q"])) {
+        
         $search=$_GET["q"];
     };
   ?>
@@ -21,7 +24,7 @@
  $token =
 'ee61f01c22063654702253187733e60145f9b4d5ae986c727112a1ff093aeccd3e76b00f42612f6554793';
 $query = 
-file_get_contents("https://api.vk.com/method/newsfeed.search?q=".$search."&count=2&access_token=".$token);
+file_get_contents("https://api.vk.com/method/newsfeed.search?q=".$search."&count=3&access_token=".$token);
 $result = json_decode($query,true);
     
 foreach($result['response'] as $value){
