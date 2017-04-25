f0a24e149be89e33419fa79e271ac2b5ef005842
Gortek2214 <nikita.borisov.36@gmail.com>
2017-04-25 20:52:41 +0300

    test button

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,17 +6,23 @@
     <title>Document</title>
 </head>
 <body>
+ 
+  <form action="/"> 
+  <input type="text" name="q">
+  <input type="submit">
+  </form>
+  
    
     <?php
  $token =
 'ee61f01c22063654702253187733e60145f9b4d5ae986c727112a1ff093aeccd3e76b00f42612f6554793';
 $query = 
-file_get_contents("https://api.vk.com/method/newsfeed.search?q=Ребята,120000&count=2&access_token=".$token);
+file_get_contents("https://api.vk.com/method/newsfeed.search?q=12&count=2&access_token=".$token);
 $result = json_decode($query,true);
-    print_r($result);
-/*foreach($result['response'] as $value){
+    
+foreach($result['response'] as $value){
     echo $value['text'];
-}*/
+}
     ?>
     
  
