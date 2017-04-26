753a657f528878751d7fcde7629b73494f401e9e
Gortek2214 <nikita.borisov.36@gmail.com>
2017-04-26 09:25:31 +0300

    test count

--- index.php
+++ index.php
@@ -9,13 +9,12 @@
 
  <?php 
    $_GET["q"] = str_replace(' ', '&shy;', $_GET["q"]);
-    if (isset($_GET["q"])) {
-        
-        $search=$_GET["q"];
-    };
+    if (isset($_GET["q"])) { $q=$_GET["q"];}
+    if (isset($_GET["col"])) { $col=$_GET["col"];}
   ?>
   <form action="/"> 
   <input type="text" name="q">
+  <input type="text" name="col">
   <input type="submit">
   </form>
   
@@ -24,11 +23,15 @@
  $token =
 'ee61f01c22063654702253187733e60145f9b4d5ae986c727112a1ff093aeccd3e76b00f42612f6554793';
 $query = 
-file_get_contents("https://api.vk.com/method/newsfeed.search?q=".$search."&count=3&access_token=".$token);
+file_get_contents("https://api.vk.com/method/newsfeed.search?q=".$q."&count=".$col."&access_token=".$token);
 $result = json_decode($query,true);
     
 foreach($result['response'] as $value){
-    echo $value['text'];
+    echo "
+    <div class='wrapp'>
+      <p>  ".$value['text']."</p>
+      <br>
+    </div>";
 }
     ?>
     
